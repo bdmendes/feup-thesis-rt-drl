2e1428e7c910b16359b04ef4c62727b0ab54b0c0
Bruno Mendes <bd_mendes@outlook.com>
2024-03-20 18:36:22 +0000

    Add mode change test

--- src/simulator/schedulability.rs
+++ src/simulator/schedulability.rs
@@ -42,7 +42,6 @@ fn response_time(
 }
 
 fn feasible_in_mode(tasks: &Vec<SimulatorTask>, mode: SimulatorMode) -> bool {
-    print!("aa");
     let eligible_tasks = match mode {
         SimulatorMode::LMode => tasks.clone(),
         SimulatorMode::HMode => tasks
@@ -65,14 +64,77 @@ fn feasible_in_mode(tasks: &Vec<SimulatorTask>, mode: SimulatorMode) -> bool {
     true
 }
 
+/// As per "Response-Time Analysis for Mixed Criticality Systems" (2011).
+/// This calculates the response time during mode changes in AMC,
+/// and ensures Ri > Ti for each HTask.
+fn response_time_in_mode_changes(task: &SimulatorTask, tasks: &Vec<SimulatorTask>) -> Option<u32> {
+    if !matches!(task.task, Task::HTask(_)) {
+        return None;
+    }
+
+    let mut total_response_time = wcet_in_mode(task.task, SimulatorMode::HMode);
+
+    let interference_by_ltasks = tasks
+        .iter()
+        .filter(|t| !matches!(t.task, Task::HTask(_)) && t.priority < task.priority)
+        .map(|t| {
+            ((response_time(task, tasks, SimulatorMode::LMode).unwrap() as f32)
+                / t.task.props().period as f32)
+                .ceil() as u32
+                * wcet_in_mode(t.task, SimulatorMode::LMode)
+        })
+        .sum::<u32>();
+
+    for _ in 0..100 {
+        let interference_by_htasks = tasks
+            .iter()
+            .filter(|t| matches!(t.task, Task::HTask(_)) && t.priority < task.priority)
+            .map(|t| {
+                (total_response_time as f32 / t.task.props().period as f32).ceil() as u32
+                    * wcet_in_mode(t.task, SimulatorMode::HMode)
+            })
+            .sum::<u32>();
+
+        let new_total_response_time = wcet_in_mode(task.task, SimulatorMode::HMode)
+            + interference_by_htasks
+            + interference_by_ltasks;
+
+        if new_total_response_time == total_response_time {
+            return Some(new_total_response_time);
+        } else {
+            total_response_time = new_total_response_time;
+        }
+    }
+
+    None
+}
+
 fn feasible_mode_changes(tasks: &Vec<SimulatorTask>) -> bool {
+    let eligible_tasks = tasks
+        .iter()
+        .cloned()
+        .filter(|t| matches!(t.task, Task::HTask(_)))
+        .collect::<Vec<_>>();
+
+    for task in &eligible_tasks {
+        if let Some(response_time) = response_time_in_mode_changes(&task, &eligible_tasks) {
+            if response_time > task.task.props().period {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
     true
 }
 
 #[cfg(test)]
 mod tests {
     use crate::simulator::{
-        schedulability::{feasible_in_mode, response_time},
+        schedulability::{
+            feasible_in_mode, feasible_mode_changes, response_time, response_time_in_mode_changes,
+        },
         task::TaskProps,
         SimulatorTask,
     };
@@ -290,5 +352,92 @@ mod tests {
             &tasks,
             crate::simulator::SimulatorMode::LMode
         ));
+    }
+
+    #[test]
+    fn feasible_mode_change_1() {
+        let task1 = SimulatorTask {
+            task: &crate::simulator::task::Task::HTask(TaskProps {
+                id: 1,
+                wcet_l: 3,
+                wcet_h: 4,
+                offset: 0,
+                period: 8,
+            }),
+            priority: 3,
+            expected_execution_time: 0,
+        };
+        let task2 = SimulatorTask {
+            task: &crate::simulator::task::Task::LTask(TaskProps {
+                id: 2,
+                wcet_l: 2,
+                wcet_h: 2,
+                offset: 0,
+                period: 8,
+            }),
+            priority: 1,
+            expected_execution_time: 0,
+        };
+        let task3 = SimulatorTask {
+            task: &crate::simulator::task::Task::LTask(TaskProps {
+                id: 3,
+                wcet_l: 2,
+                wcet_h: 2,
+                offset: 0,
+                period: 8,
+            }),
+            priority: 2,
+            expected_execution_time: 0,
+        };
+
+        let tasks = vec![task1, task2, task3];
+
+        assert_eq!(response_time_in_mode_changes(&task1, &tasks,), Some(8));
+
+        assert!(feasible_mode_changes(&tasks));
+    }
+
+    #[test]
+    fn feasible_mode_change_2() {
+        let task1 = SimulatorTask {
+            task: &crate::simulator::task::Task::HTask(TaskProps {
+                id: 1,
+                wcet_l: 3,
+                wcet_h: 4,
+                offset: 0,
+                period: 8,
+            }),
+            priority: 3,
+            expected_execution_time: 0,
+        };
+        let task2 = SimulatorTask {
+            task: &crate::simulator::task::Task::HTask(TaskProps {
+                id: 2,
+                wcet_l: 2,
+                wcet_h: 2,
+                offset: 0,
+                period: 8,
+            }),
+            priority: 1,
+            expected_execution_time: 0,
+        };
+        let task3 = SimulatorTask {
+            task: &crate::simulator::task::Task::LTask(TaskProps {
+                id: 3,
+                wcet_l: 2,
+                wcet_h: 2,
+                offset: 0,
+                period: 8,
+            }),
+            priority: 2,
+            expected_execution_time: 0,
+        };
+
+        let tasks = vec![task1, task2, task3];
+
+        assert_eq!(response_time_in_mode_changes(&task1, &tasks,), Some(8));
+        assert_eq!(response_time_in_mode_changes(&task2, &tasks,), Some(2));
+
+        assert!(feasible_mode_changes(&tasks));
     }
 }
